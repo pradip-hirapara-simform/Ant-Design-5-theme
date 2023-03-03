9aceb69ca3fe7f670739c39b5b045729d2961bfa
Pradip Hirapara <pradip.h@simformsolutions.com>
2023-03-03 16:51:16 +0530

    added GlobalStyle

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import Home from "./Pages/Home/Home";
 import { ThemeProvider } from "styled-components";
-import { useColorConfig } from "./ThemeConfig/UseColorConfig";
- import GlobalStyle from "./Global.style";
+import { useColorConfig } from "./ThemeConfig/UseColorConfig"; 
 import About from "./Pages/About/About";
+import GlobalStyle from './Global.style';
 import Layout from "./Layout/Layout";
 import { antDComponents, antDTheme, customTheme } from "./ThemeConfig/Variables";
 
